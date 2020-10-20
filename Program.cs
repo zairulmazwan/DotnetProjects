64a0eef54a0ccd805e1c76fca016eae600f34361
zm1142 <zm1142@STA-DK-400392>
2020-10-20 10:52:36 +0100

    Fixed the document and page error

--- Program.cs
+++ Program.cs
@@ -11,20 +11,21 @@ namespace DotnetProjects
 
 
             Document doc = new Document(); // creating a document object
-            Section section = new Section(); //creating a section in the document object
+            Section section = doc.AddSection(); //creating a section in the document object
 
             Paragraph para = section.AddParagraph(); //adding a paragraph for the section
             para.Format.Font.Name = "Arial";
             para.Format.Font.Size = 26;
             para.Format.Font.Color = Color.FromCmyk(0,0,0,100);
-            para.AddFormattedText("Hello Zairul Mazwan", TextFormat.Italic);
+            para.AddFormattedText("Hello Zairul Mazwan", TextFormat.Bold);
             para.Format.SpaceAfter = "1.0cm";
 
             Paragraph para1 = section.AddParagraph(); //adding a paragraph for the section
             para1.Format.Font.Name = "Arial";
             para1.Format.Font.Size = 18;
             para1.Format.Font.Color = Color.FromCmyk(0, 0, 0, 100);
-            para1.AddFormattedText("Welcome to my first PDF file", TextFormat.Bold);
+            para1.Format.Font.Italic = true;
+            para1.AddFormattedText("Welcome to my first PDF file");
             para1.Format.SpaceAfter = "1.0cm";
 
 
@@ -32,9 +33,10 @@ namespace DotnetProjects
             PdfDocumentRenderer pdfRend = new PdfDocumentRenderer();
             pdfRend.Document = doc;
             pdfRend.RenderDocument();
-            pdfRend.PdfDocument.Save("MyFile.pdf");
+            pdfRend.PdfDocument.Save("C:\\Users\\zm1142\\DotnetProjects\\MyFile.pdf");
 
             Console.WriteLine("File Saved");
+            Console.ReadLine();
 
         }
     }
